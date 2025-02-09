e45abfed1dfde843e1c48bca5c3c07bc86668da0
nourdd07 <safir8522@safirschool.k12.tr>
2025-02-09 21:47:05 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,3 +7,28 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
 });
+
+// Timer Script
+const countDownDate = new Date("May 9, 2025 00:00:00").getTime();
+
+const timer = setInterval(() => {
+  const now = new Date().getTime();
+  const distance = countDownDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("timer").innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+
+  if (distance < 0) {
+    clearInterval(timer);
+    document.getElementById("timer").innerHTML = "EVENT STARTED";
+  }
+}, 1000);
+
+// Toggle the visibility of the navbar
+function toggleMenu() {
+  document.querySelector(".nav-bar").classList.toggle("show");
+}
